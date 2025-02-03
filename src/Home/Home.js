1054f0bbf763787f718a8c5d817b9652027f3623
lee_jihoon <jihun629@gmail.com>
2025-02-03 11:24:06 +0900

    예시 추가

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,6 +1,14 @@
 import React from "react";
 import "../Style/Home.css";
-
+import "../Style/RayOut.css";
 export default function Home() {
-  return <div>게시글 동네생활 etc...</div>;
+  return (
+    <div>
+      <h1>당근마켓</h1>
+      <div className="nav">
+        <div>게시글 동네생활</div>
+        <div>내 근처 채팅</div>
+      </div>
+    </div>
+  );
 }
