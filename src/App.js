ef8d2909faa8e9e3948ead4dce1eee08c25727d3
Blue-B <source_vs@naver.com>
2025-02-04 13:31:08 +0900

    Feat: 내용 병합

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React, { useState } from "react";
 import Home from "./Component/Home";
-
+import Detail from "./Component/Detail";
+import Write from "./Component/Write";
+import Comunity from "./Component/Comunity";
 export default function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+  const [currentPage, setCurrentPage] = useState("home");
+
+  const goBack = () => {
+    setCurrentPage("home");
+    // renderPage();
+  };
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case "home":
+        return (
+          <Home
+            onWriteClick={() => setCurrentPage("write")}
+            onDetailClick={() => setCurrentPage("detail")}
+            onComunityClick={() => setCurrentPage("comunity")}
+          />
+        );
+      // return <Home MoveWrite={MoveWrite} MoveComunity={MoveComunity} />;
+
+      case "detail":
+        return <Detail goBack={goBack} />;
+      case "write":
+        return <Write goBack={goBack} />;
+      case "comunity":
+        return <Comunity goBack={goBack} />;
+      default:
+        return <Home />;
+    }
+  };
+  return <div>{renderPage()}</div>;
 }
