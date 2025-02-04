a7ab988b9b6840d0e160ef2a7f110ffdc1aef44d
lee_jihoon <jihun629@gmail.com>
2025-02-04 10:21:51 +0900

    ㅁㅁㅁ

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./Style/index.css";
 import reportWebVitals from "./reportWebVitals";
-import Home from "./Home/Home";
-
+import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>
 );
 
