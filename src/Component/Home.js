42c50013baa1a31e495acb2ce13831b04ee39ede
lee_jihoon <jihun629@gmail.com>
2025-02-04 14:55:37 +0900

    home 예시 map 써서 div들 생성해주세요

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -7,12 +7,17 @@ export default function Home({
   onDetailClick,
   posts,
 }) {
+  console.log(posts);
   return (
     <div>
       <h1>당근마켓</h1>
       <div className="nav">
         <div className="detail" onClick={() => onDetailClick()}>
           이거 누르면 세부정보로 이동해요
+        </div>
+
+        <div className="detail" onClick={() => onDetailClick()}>
+          최신 항목 {posts.length > 0 ? posts[0].name : ""}
         </div>
       </div>
       <button onClick={onWriteClick}> 글쓰기 </button>
