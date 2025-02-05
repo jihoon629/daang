220aff4f6888d09350e6128ef28c00aa0e4552b9
lee_jihoon <jihun629@gmail.com>
2025-02-05 11:11:27 +0900

    커뮤니티 템플릿 완성

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Detail from "./Component/Detail";
 import Write from "./Component/Write";
 import Comunity from "./Component/Comunity";
 import ComunityWrite from "./Component/ComunityWrite"; //커뮤니티 글쓰기 추가
+import ComunityBorad from "./Component/ComunityBorad";
 
 export default function App() {
   const [currentPage, setCurrentPage] = useState("home");
   const [posts, setPosts] = useState([]);
   const [comunityPosts, setComunityPosts] = useState([]);
   const [selectedPostId, setSelectedPostId] = useState(null);
+  const [selectedBoardId, setSelectedBoardId] = useState(null);
 
   useEffect(() => {
     // 로컬스토리지에서 게시글 불러오기
@@ -17,8 +19,12 @@ export default function App() {
     setPosts(savedPosts);
   }, []);
 
-  const goBack = () => {
+  const goBackHome = () => {
     setCurrentPage("home");
+  };
+
+  const goBackComunity = () => {
+    setCurrentPage("comunity");
   };
 
   const addPosts = (newpost) => {
@@ -28,12 +34,12 @@ export default function App() {
     setCurrentPage("home");
   };
 
-  const addComunityPost = (title, content) => {
-    const newPost = { title, content };
+  const addComunityPost = (id, title, content) => {
+    const newPost = { id, title, content };
     const updateComunityPosts = [newPost, ...comunityPosts];
     setComunityPosts(updateComunityPosts);
     localStorage.setItem("comunityPosts", JSON.stringify(updateComunityPosts));
-    setCurrentPage("comunity");  // 글 작성 후 커뮤니티 페이지로 이동
+    setCurrentPage("comunity"); // 글 작성 후 커뮤니티 페이지로 이동
   };
 
   const deletePosts = () => {};
@@ -51,13 +57,35 @@ export default function App() {
         );
 
       case "detail":
-        return <Detail goBack={goBack} />;
+        return <Detail goBack={goBackHome} />;
       case "write":
-        return <Write goBack={goBack} onSubmit={addPosts} />;
-      case "comunity_write":  // 새로운 ComunityWrite 페이지로 이동
-        return <ComunityWrite addPost={addComunityPost} goBack={goBack} />;
+        return <Write goBack={goBackHome} onSubmit={addPosts} />;
+      case "comunity_write": // 새로운 ComunityWrite 페이지로 이동
+        return (
+          <ComunityWrite
+            addPost={addComunityPost}
+            goBackComunity={goBackComunity}
+          />
+        );
       case "comunity":
-        return <Comunity goBack={goBack} posts={comunityPosts} onComunity_write={() => setCurrentPage("comunity_write")} onDelete={deletePosts} />;
+        return (
+          <Comunity
+            goBackHome={goBackHome}
+            posts={comunityPosts}
+            onComunity_write={() => setCurrentPage("comunity_write")}
+            onBoardClick={(BoardId) => {
+              setSelectedBoardId(BoardId);
+              setCurrentPage("boardDetail");
+            }}
+            onDelete={deletePosts}
+          />
+        );
+      case "boardDetail":
+        return (
+          <ComunityBorad
+            post={comunityPosts.find((p) => p.id === selectedBoardId)}
+          />
+        );
       default:
         return <Home />;
     }
