9021da1e3dd615576a6b6bdad3e203489223bbc2
Blue-B <55532956+Blue-B@users.noreply.github.com>
2025-02-05 22:13:26 +0900

    Refactor: 파일 통합
    Home페이지 코드 통합 스타일 통합

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "../Style/Home.css";
 import "../Style/RayOut.css";
-import { FaPen } from "react-icons/fa";
-import { MdFeed } from "react-icons/md";
 
 export default function Home({
   onWriteClick,
@@ -13,7 +11,7 @@ export default function Home({
   console.log(posts);
   return (
     <div>
-      <h1>당근마켓</h1>
+      <h1 className="page-title">당근마켓</h1>
       <div className="nav">
         <div>
           {posts.map((post) => (
@@ -22,22 +20,26 @@ export default function Home({
               className="detail"
               onClick={() => onDetailClick(post.id)}
             >
-              <img src={post.image} alt="" /> {/*물품 이미지*/}
-              {post.name} {/*물품 이름*/}
-              가격: {post.price} {/*물품 가격*/}
-              네고 여부 (아이콘)
+              <img src={post.image} alt="상품 이미지" className="detail-img" />
+              <div className="detail-info">
+                <p className="detail-name">{post.name}</p>
+                <p className="detail-price">가격: {post.price}원</p>
+                <p className="detail-negotiable">
+                  {post.priceNegotiable ? "네고 가능" : "네고 불가"}
+                </p>
+              </div>
             </div>
           ))}
         </div>
       </div>
-            {/* <FaPen onClick={onWriteClick}/>
-            <MdFeed onClick={onComunityClick}/> */}
-            
-      <div className="bottom-navbar">
-        <FaPen size="30" title="글쓰기" onClick={onWriteClick} />
-        <MdFeed size="30" title="커뮤니티"  onClick={onComunityClick} />
+      <div className="button-container">
+        <button className="action-button" onClick={onWriteClick}>
+          글쓰기
+        </button>
+        <button className="action-button" onClick={onComunityClick}>
+          커뮤니티
+        </button>
       </div>
-   
     </div>
   );
 }
