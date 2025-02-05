43ea06417f4cbd65e2bb408723e33445aabdac47
PARKSEONAH251 <161944840+PARKSEONAH251@users.noreply.github.com>
2025-02-05 21:33:17 +0900

    ComunityBorad CSS 작업 및 수정

--- src/Component/ComunityBorad.js
+++ src/Component/ComunityBorad.js
@@ -1,17 +1,18 @@
 import React from "react";
+import { IoArrowBack } from "react-icons/io5";
+import "../Style/comunityBoard.css"; // CSS 파일 임포트
 
-export default function ComunityBorad({ post, goBackComunity, onDelete }) {
-  const handleDelete = () => {
-    onDelete(post.id);
-  };
-
+export default function ComunityBoard({ post, goBackComunity }) {
   return (
-    <div>
-      id: {post.id}
-      <h1>제목:{post.title}</h1>
-      내용: {post.content}
-      <button onClick={goBackComunity}>뒤로가기</button>
-      <button onClick={handleDelete}>삭제</button>
+    <div className="main-container">
+      <div className="backicon" onClick={goBackComunity}> 
+        <IoArrowBack size="25" title="뒤로가기"/> 커뮤니티
+      </div>
+      <div className="post-container">
+        <p className="post-id">작성자 ID: {post.id}</p>
+        <h1 className="post-title">{post.title}</h1>
+        <p className="post-content">{post.content}</p>
+      </div>
     </div>
   );
 }
