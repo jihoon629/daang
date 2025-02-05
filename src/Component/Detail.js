1c445aacd5665538368117b45afc5eab750cc96e
Blue-B <55532956+Blue-B@users.noreply.github.com>
2025-02-05 21:52:57 +0900

    Style:detail페이지 수정

--- src/Component/Detail.js
+++ src/Component/Detail.js
@@ -13,16 +13,21 @@ export default function Detail({ goBack, post, onDelete }) {
   };
 
   return (
-    <div>
-      <h1>{post.name}</h1>
-      {post.image && (
-        <img src={post.image} alt="상품 이미지" style={{ width: "200px" }} />
-      )}
-      <p>가격: {post.price}원</p>
-      <p>{post.priceNegotiable ? "가격 제안 가능" : "가격 제안 불가"}</p>
-      <p>설명: {post.description}</p>
-      <button onClick={goBack}>뒤로 가기</button>
-      <button onClick={handleDelete}>판매 완료</button>
+    <div className="fied">
+      <div className="detail">
+        <h1 className="detail-header">{post.name}</h1>
+        {post.image && (
+          <img src={post.image} alt="상품 이미지" className="detail-image" />
+        )}
+        <p className="detail-price">가격: {post.price}원</p>
+        <p>{post.priceNegotiable ? "가격 제안 가능" : "가격 제안 불가"}</p>
+        <div className="detail-description">설명: {post.description}</div>
+      </div>
+
+      <div className="button-container">
+          <button className="detail-button" onClick={goBack}>뒤로 가기</button>&nbsp;&nbsp;&nbsp;
+          <button className="detail-button" onClick={handleDelete}>판매 완료</button>
+        </div>
     </div>
   );
 }
