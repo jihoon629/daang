a876361e86cff8c369cac5293fd2c2a3e568db31
lee_jihoon <jihun629@gmail.com>
2025-02-05 14:07:20 +0900

    dd

--- src/Component/Detail.js
+++ src/Component/Detail.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-export default function detail({ goBack }) {
+export default function detail({ goBack, post }) {
   return (
     <div>
       <h1>디테일</h1>
-
+      {post.name}
+      {post.description}
       <button onClick={goBack}> 취소 </button>
     </div>
   );
