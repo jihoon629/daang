14da7ae6ec3166dbb30641b3c36da0f5b2a151f0
Blue-B <source_vs@naver.com>
2025-02-05 15:48:42 +0900

    Refactor: Detail.js 파일 위치 수정

--- src/Component/Detail.js
+++ src/Component/Detail.js
@@ -1,12 +1,21 @@
-import React from "react";
+export default function Detail({ goBack, post }) {
+  if (!post) {
+    return (
+      <div>
+        <h1>게시글을 찾을 수 없습니다.</h1>
+        <button onClick={goBack}>뒤로 가기</button>
+      </div>
+    );
+  }
 
-export default function detail({ goBack, post }) {
   return (
     <div>
-      <h1>디테일</h1>
-      {post.id}
-
-      <button onClick={goBack}> 취소 </button>
+      <h1>{post.name}</h1>
+      {post.image && <img src={post.image} alt="상품 이미지" style={{ width: "200px" }} />}
+      <p>가격: {post.price}원</p>
+      <p>{post.priceNegotiable ? "가격 제안 가능" : "가격 제안 불가"}</p>
+      <p>설명: {post.description}</p>
+      <button onClick={goBack}>뒤로 가기</button>
     </div>
   );
 }
